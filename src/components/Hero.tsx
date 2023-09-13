c8a8dbe04922232a37a5eb8725090062429305b1
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-13 18:49:39 +0530

    Update hero margins

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="mt-32 font-thin text-center">
+    <div className="mt-16 md:mt-32 font-thin text-center">
       <h1 className="text-6xl">simple-txt</h1>
       <p className="mt-4 text-lg">A simple note taking app.</p>
       <p className="mt-16 text-primary text-sm">
