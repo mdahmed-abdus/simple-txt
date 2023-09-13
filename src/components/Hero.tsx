4a2bed7ecb6c006a30fc56a3db319d9fab171e9a
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-13 11:24:39 +0530

    Update hero css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
 export default function Hero() {
   return (
-    <div className="mt-32 mb-32 font-thin text-center">
+    <div className="mt-32 font-thin text-center">
       <h1 className="text-6xl">simple-txt</h1>
       <p className="mt-4 text-lg">A simple note taking app.</p>
-      <p className="mt-8 text-primary text-sm text-center">
+      <p className="mt-8 text-primary text-sm">
         Designed and developed by @
         <a
           className="hover:text-destructive hover:underline"
