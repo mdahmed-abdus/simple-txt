73781145e7b88346e58f4f6dd415bebdfa3b4684
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-15 16:41:47 +0530

    Add toast ui component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
+import { Toaster } from '@/components/ui/toaster';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={rubik.className + ' container'}>
         <Navbar />
         <div>{children}</div>
+        <Toaster />
       </body>
     </html>
   );
