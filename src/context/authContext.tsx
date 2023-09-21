302299b72e5d78dd95852d3f493da1a5e5af5933
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-21 15:31:12 +0530

    Update AuthContext

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -4,17 +4,22 @@ import { createContext, useContext, useState } from 'react';
 
 export const AuthContext = createContext<{
   authStatus: boolean;
+  user: { name: string; email: string };
   setAuthStatus: (status: boolean) => void;
+  setUser: ({ name, email }: { name: string; email: string }) => void;
 }>({
   authStatus: false,
+  user: { name: '', email: '' },
   setAuthStatus: () => {},
+  setUser: () => {},
 });
 
 export const AuthContextProvider = ({ children }: any) => {
   const [authStatus, setAuthStatus] = useState(false);
+  const [user, setUser] = useState({ name: '', email: '' });
 
   return (
-    <AuthContext.Provider value={{ authStatus, setAuthStatus }}>
+    <AuthContext.Provider value={{ authStatus, user, setAuthStatus, setUser }}>
       {children}
     </AuthContext.Provider>
   );
