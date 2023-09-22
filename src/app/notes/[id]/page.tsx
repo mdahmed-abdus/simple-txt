0e8e9b2efb834f9244eccba6dd82e496255d39a5
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-22 16:05:46 +0530

    Redirect to dashboard if note with id not found

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -48,6 +48,7 @@ export default function NoteById({ params }: { params: { id: string } }) {
       .catch(error => {
         toast({ variant: 'destructive', description: error.message });
         setLoading(false);
+        router.push('/dashboard');
       });
   };
 
