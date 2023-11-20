6f6e35feea9f07fe3ccba490b2b47c35bd005373
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-11-20 19:06:00 +0530

    Prevent logout to unauthorized users

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,10 +1,19 @@
 import { connectDb } from '@/services/db';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { isLoggedIn } from '../../helpers/auth';
 
 connectDb();
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
+    // prevent access to unauthorized users
+    if (!isLoggedIn(request)) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
     const response = NextResponse.json(
       { success: true, message: 'User logged out' },
       { status: 200 }
