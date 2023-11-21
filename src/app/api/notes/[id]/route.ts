a4485b70f4aa3a588d59906057b61e60801de92f
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-11-21 11:02:02 +0530

    Implement all api routes for notes

---
+++ src/app/api/notes/[id]/route.ts
@@ -0,0 +1,129 @@
+import { User } from '@/models/User';
+import { connectDb } from '@/services/db';
+import { NextRequest, NextResponse } from 'next/server';
+import { isLoggedIn } from '../../helpers/auth';
+
+connectDb();
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const userId = isLoggedIn(request);
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findById(userId);
+    const note = user.notes.find(
+      (n: any, i: number) => n._id.toString() === params.id
+    );
+
+    if (!note) {
+      return NextResponse.json(
+        { success: false, message: 'Note with given id was not found' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: 'Note found', note },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const userId = isLoggedIn(request);
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findById(userId);
+    const note = user.notes.find(
+      (n: any, i: number) => n._id.toString() === params.id
+    );
+
+    if (!note) {
+      return NextResponse.json(
+        { success: false, message: 'Note with given id was not found' },
+        { status: 404 }
+      );
+    }
+
+    const reqBody = await request.json();
+
+    // if updated title not provided then use the old title
+    // if updated body not provided then use the old body
+    const { title = note.title, body = note.body } = reqBody;
+
+    note.title = title;
+    note.body = body;
+    await user.save();
+
+    return NextResponse.json(
+      { success: true, message: 'Note updated', note },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const userId = isLoggedIn(request);
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findById(userId);
+    const note = user.notes.find(
+      (n: any, i: number) => n._id.toString() === params.id
+    );
+
+    if (!note) {
+      return NextResponse.json(
+        { success: false, message: 'Note with given id was not found' },
+        { status: 404 }
+      );
+    }
+
+    user.notes = user.notes.filter(
+      (note: any) => note._id.toString() !== params.id
+    );
+    await user.save();
+
+    return NextResponse.json(
+      { success: true, message: 'Note deleted', note },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json(
+      { success: false, message: 'Something went wrong' },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -2,6 +2,7 @@ import { User } from '@/models/User';
 import { connectDb } from '@/services/db';
 import { NextRequest, NextResponse } from 'next/server';
 import { isLoggedIn } from '../helpers/auth';
+import { Note } from '@/models/Note';
 
 connectDb();
 
@@ -20,6 +21,35 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(
       { success: true, message: `Found ${notes.length} note(s)`, notes },
       { status: 200 }
+    );
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const userId = isLoggedIn(request);
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const reqBody = await request.json();
+    const { title, body } = reqBody;
+
+    const user = await User.findById(userId);
+    const note = new Note({ title, body });
+
+    user.notes.push(note);
+    await user.save();
+
+    return NextResponse.json(
+      { success: true, message: 'Note created', note },
+      { status: 201 }
     );
   } catch (error: any) {
     console.log(error);
