0dd8e4ce7c26b4c88fd941b24dcfd767aff3a475
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-11-21 11:38:26 +0530

    Update types in mail service

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -1,9 +1,8 @@
 import { Resend } from 'resend';
-import { CreateEmailOptions } from 'resend/build/src/emails/interfaces';
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 const FROM = 'simple-txt noreply <simple-txt@resend.dev>';
 
-export const sendMail = (options: CreateEmailOptions) =>
+export const sendMail = (options: any) =>
   resend.emails.send({ ...options, from: FROM });
