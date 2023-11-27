8f84bb9725614a66da8a9ae8fe9a17806a4694c4
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-11-27 15:54:26 +0530

    Update dashboard page ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -39,8 +39,7 @@ export default function Dashboard() {
       <h1 className="text-3xl text-center">
         Welcome back, <span className="font-normal">{user.name}</span>
       </h1>
-
-      <Table className="mt-16">
+      <Table className="mt-16 hidden sm:table">
         <TableCaption>
           {notes.length > 0 ? (
             'A list of your notes.'
@@ -62,7 +61,7 @@ export default function Dashboard() {
             <TableHead>Body</TableHead>
             <TableHead className="text-right">
               <Button
-                className="font-thin"
+                className="p-0 font-thin"
                 variant="link"
                 onClick={() => router.push('notes/new')}
               >
@@ -76,7 +75,9 @@ export default function Dashboard() {
             <TableRow key={`dashboardTableRow_${_id}`}>
               <TableCell>{index + 1}</TableCell>
               <TableCell className="font-normal">{title}</TableCell>
-              <TableCell>{body.slice(0, 100) + '...'}</TableCell>
+              <TableCell>
+                {body.length > 100 ? body.slice(0, 100) + '...' : body}
+              </TableCell>
               <TableCell className="text-right">
                 <Button
                   className="font-thin"
@@ -85,6 +86,48 @@ export default function Dashboard() {
                 >
                   View
                 </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Table className="mt-16 table sm:hidden">
+        <TableCaption>
+          {notes.length > 0 ? (
+            'A list of your notes.'
+          ) : (
+            <div>
+              <p>You do not have any notes.</p>
+              <p>
+                Click on{' '}
+                <span className="inline font-normal">Add new note</span> to add
+                a new note.
+              </p>
+            </div>
+          )}
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              <Button
+                className="p-0 font-thin"
+                variant="link"
+                onClick={() => router.push('notes/new')}
+              >
+                Add new note
+              </Button>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {notes.map(({ _id, title, body }, index) => (
+            <TableRow key={`dashboardTableRow_${_id}`}>
+              <TableCell onClick={() => router.push(`notes/${_id}`)}>
+                <p className="text-xs">No. {index + 1}</p>
+                <p className="font-normal mt-2">{title}</p>
+                <p className="mt-1">
+                  {body.length > 100 ? body.slice(0, 100) + '...' : body}
+                </p>
               </TableCell>
             </TableRow>
           ))}
